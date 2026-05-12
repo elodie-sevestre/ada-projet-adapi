de137b0ec8ac0065fe9d08421f928192f701487d
Elodie S <elodie.sevestre86@gmail.com>
2026-05-12 12:04:14 +0200

    route resources done

--- notes/instructions.md
+++ notes/instructions.md
@@ -5,4 +5,6 @@
 [x] Créer une route POST pour les skills
 [x] Créer une route PUT pour les skills
 [x] Créer une route DELETE pour les skills
-[ ] Créer les même routes pour les themes / resources / resources_skills
+[ ] Créer les même routes pour les themes
+[ ] Créer les même routes pour les resources
+[ ] Créer les même routes pour les resources_skills
--- src/server.js
+++ src/server.js
@@ -96,45 +96,102 @@ app.delete("/themes/:id", async (req, res) => {
 
 // ==================== Table : resources ====================
 
-// GET /themes - création route GET pour récupérer les themes
-app.get("/themes", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM themes ORDER BY id"); // on aurait pu écrire const results = ...
-  res.json(rows); // et ici res.json(results.rows)
+// GET /resources - création route GET pour récupérer les resources
+app.get("/resources", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM resources ORDER BY id");
+  res.json(rows);
 });
 
-// GET /themes/:id - création route GET pour récupérer une themes par son id
-app.get("/themes/:id", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM themes WHERE id = $1 ", [
+// GET /resources/:id - création route GET pour récupérer une resources par son id
+app.get("/resources/:id", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM resources WHERE id = $1 ", [
     req.params.id,
   ]);
   res.json(rows[0]);
 });
 
-// POST /themes - création route POST pour insérer une nouvelle themes
-app.post("/themes", async (req, res) => {
-  const insertNewThemes = await pool.query(
-    "INSERT INTO themes (name) VALUES ($1) RETURNING *",
+// POST /resources - création route POST pour insérer une nouvelle resources
+app.post("/resources", async (req, res) => {
+  const insertNewResources = await pool.query(
+    "INSERT INTO resources (name) VALUES ($1) RETURNING *",
     [req.body.name],
   );
-  res.json(insertNewThemes.rows[0]);
+  res.json(insertNewResources.rows[0]);
 });
 
-// PUT /themes - création route PUT pour remplacer le nom d'une themes par son id
-app.put("/themes/:id", async (req, res) => {
-  const replaceThemesName = await pool.query(
-    "UPDATE themes SET name = $1 WHERE id = $2 RETURNING *",
+// PUT /resources - création route PUT pour remplacer le nom d'une resources par son id
+app.put("/resources/:id", async (req, res) => {
+  const replaceResourcesName = await pool.query(
+    "UPDATE resources SET name = $1 WHERE id = $2 RETURNING *",
     [req.body.name, req.params.id],
   );
-  res.json(replaceThemesName.rows[0]);
+  res.json(replaceResourcesName.rows[0]);
 });
 
-// DELETE - création route DELETE pour supprimer une themes par son id
-app.delete("/themes/:id", async (req, res) => {
-  const deleteThemes = await pool.query(
-    " DELETE FROM themes WHERE id=$1 RETURNING *",
+// DELETE - création route DELETE pour supprimer une resources par son id
+app.delete("/resources/:id", async (req, res) => {
+  const deleteResources = await pool.query(
+    " DELETE FROM resources WHERE id=$1 RETURNING *",
     [req.params.id],
   );
-  res.json(deleteThemes.rows[0]);
+  res.json(deleteResources.rows[0]);
+});
+
+// ==================== Table : resources_skills ====================
+
+//! resources_skills est une table de liaison. Elle lie les tables resources et skills grâce à leur id.
+
+// GET /resources_skills - création route GET pour récupérer les resources_skills
+app.get("/resources_skills", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM resources_skills");
+  res.json(rows);
+});
+
+// GET /resources_skills/resource/:resource_id - création route GET pour récupérer une resources_skills
+// id = resource_id
+app.get("/resources_skills/resource/:resource_id", async (req, res) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM resources_skills WHERE resource_id = $1 ",
+    [req.params.resource_id],
+  );
+  res.json(rows);
+});
+
+// GET /resources_skills/skill/:skill_id - création route GET pour récupérer une resources_skills
+// id = skill_id
+app.get("/resources_skills/skill/:skill_id", async (req, res) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM resources_skills WHERE skill_id = $1 ",
+    [req.params.skill_id],
+  );
+  res.json(rows);
+});
+
+// POST /resources_skills - création route POST pour insérer une nouvelle resources_skills
+app.post("/resources_skills", async (req, res) => {
+  const insertNewResourcesSkills = await pool.query(
+    "INSERT INTO resources_skills (name) VALUES ($1) RETURNING *",
+    [req.body.name],
+  );
+  res.json(insertNewResourcesSkills.rows);
+});
+
+// PUT /resources_skills - création route PUT pour remplacer le nom d'une resources_skills par son id
+app.put("/resources_skills/:id", async (req, res) => {
+  const replaceResourcesSkillsName = await pool.query(
+    "UPDATE resources_skills SET name = $1 WHERE id = $2 RETURNING *",
+    [req.body.name, req.params.id],
+  );
+  res.json(replaceResourcesSkillsName.rows[0]);
+});
+
+// DELETE - création route DELETE pour supprimer une resources_skills par son id
+app.delete("/resources_skills/:id", async (req, res) => {
+  const deleteResourcesSkills = await pool.query(
+    " DELETE FROM resources_skills WHERE id=$1 RETURNING *",
+    [req.params.id],
+  );
+  res.json(deleteResourcesSkills.rows[0]);
 });
 
 // Serveur express écoute port 3000, affiche "" et se connecte à la BDD
